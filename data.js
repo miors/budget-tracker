55b17024c4705b1ae062582915e69b5ca7add182
Mior Sufian <mior.sufian@googlemail.com>
2023-10-06 11:44:10 +0100

    final code

---
+++ data.js
@@ -0,0 +1,5 @@
+const transactions = [
+  { id: 123, source: "Freelancing", amount: 499, time: "ABC" },
+  { id: 125, source: "Studio", amount: -50, time: "ABC" },
+  { id: 126, source: "Market", amount: 200, time: "ABC" },
+];
